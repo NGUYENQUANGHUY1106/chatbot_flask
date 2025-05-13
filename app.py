29a8d3dff67fbd1a869ad92d94b5cafc9258203b
Nguyễn Quang Huy <nguyenquanghuy110605@gmail.com>
2025-05-14 01:22:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,7 @@ def chat():
 @app.route("/web")
 def chatbot_page():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
